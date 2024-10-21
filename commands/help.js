9ef8d8981f49e6ad4fb8c23906dc812af4b8c96e
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-21 21:41:12 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,45 +4,20 @@ const path = require('path');
 module.exports = {
   name: 'help',
   description: 'Show available commands',
-  author: 'System',
+  author: 'chilli',
   execute(senderId, args, pageAccessToken, sendMessage) {
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
-    // Create the buttons
-    const buttons = commandFiles.map((file, index) => {
+    const commands = commandFiles.map((file, index) => {
       const command = require(path.join(commandsDir, file));
-      return {
-        type: 'postback',
-        title: `${command.name}`,
-        payload: `${command.description}`
-      };
+      return `${index + 1}. ${command.name} - ${command.description}`;
     });
 
     const totalCommands = commandFiles.length;
-
-    const helpMessage = `📋 | CMD List:\n🏷 Total Commands: ${totalCommands}\n\nIf you have any problems with the pagebot, contact the developer.`;
-
-    // Prepare payload for Messenger's template
-    const payload = {
-      template_type: 'button',
-      text: helpMessage,
-      buttons: buttons.slice(0, 3) // Only 3 buttons per template allowed by Messenger API
-    };
-
-    // Add Contact Developer button
-    payload.buttons.push({
-      type: 'web_url',
-      url: 'https://www.facebook.com/Churchill.Dev4100',
-      title: 'Contact Developer'
-    });
-
-    // Send message with button template
-    sendMessage(senderId, {
-      attachment: {
-        type: 'template',
-        payload: payload
-      }
-    }, pageAccessToken);
+    
+    const helpMessage = `📋 | CMD List:\n🏷 Total Commands: ${totalCommands}\n\n${commands.join('\n\n')}\n\nIf you have any problems with the pagebot, contact the developer:\nFB Link: https://www.facebook.com/Churchill.Dev4100`;
+    
+    sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
