49b936227d77621990e855a04d87f0b4c221fedf
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-21 18:10:43 +0800

    Update imagine.js

--- commands/imagine.js
+++ commands/imagine.js
@@ -15,7 +15,7 @@ module.exports = {
     const prompt = args.join(' ');
 
     try {
-      const apiUrl = `https://ccprojectsjonellapis-production.up.railway.app/api/generate-art?prompt=${encodeURIComponent(prompt)}`;
+      const apiUrl = `https://appjonellccapis.zapto.org/api/generate-art?prompt=${encodeURIComponent(prompt)}`;
 
       await sendMessage(senderId, { attachment: { type: 'image', payload: { url: apiUrl } } }, pageAccessToken);
 
