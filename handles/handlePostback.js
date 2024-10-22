26b9abc1cf5c1629be2a395f1c8acddfedb71565
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-22 19:23:05 +0800

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,8 +4,32 @@ function handlePostback(event, pageAccessToken) {
   const senderId = event.sender.id;
   const payload = event.postback.payload;
 
-  // Send a message back to the sender
-  sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+  if (senderId && payload) {
+    if (payload === 'GET_STARTED_PAYLOAD') {
+      const welcomeMessage = {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: "Welcome to Chillibot! Ready to explore our AI and utility commands? If you want to see a list of commands, type 'help' or click the 'Commands' button below.",
+            buttons: [
+              {
+                type: "postback",
+                title: "Commands",
+                payload: "HELP"
+              }
+            ]
+          }
+        }
+      };
+
+      sendMessage(senderId, welcomeMessage, pageAccessToken);
+    } else {
+      sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+    }
+  } else {
+    console.error('Invalid postback event data');
+  }
 }
 
 module.exports = { handlePostback };
