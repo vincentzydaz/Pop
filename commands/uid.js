b4a7ae4e15ee7a8bd788ea7f88630153144a95ea
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-26 19:28:02 +0800

    Create uid.js

---
+++ commands/uid.js
@@ -0,0 +1,18 @@
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const pageAccessToken = fs.readFileSync('token.txt', 'utf8');
+
+module.exports = {
+  name: 'uid',
+  author: 'coffee',
+  
+  async execute(senderId) {
+    try {
+      await sendMessage(senderId, { text: `${senderId}` }, pageAccessToken);
+    } catch (error) {
+      console.error('Error sending UID:', error);
+      await sendMessage(senderId, { text: 'Error: Unable to retrieve your UID.' }, pageAccessToken);
+    }
+  },
+};
