e759791f15540b7a4e369f75e2a37b87044e8bd8
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-27 16:04:14 +0800

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -9,23 +9,19 @@ module.exports = {
   async execute(senderId, args, pageAccessToken, event) {
     const adminId = '8731046750250922';
 
-    // Check if the sender is the admin
     if (senderId !== adminId) {
       return sendMessage(senderId, { text: 'Admin access only.' }, pageAccessToken);
     }
 
     try {
-      // Notify the admin that the bot is restarting
       await sendMessage(senderId, { text: 'Bot is restarting...' }, pageAccessToken);
       
-      // Write restart information to a file
       const restartFile = './restart.json';
       fs.writeFileSync(restartFile, JSON.stringify({ restartId: senderId, time: Date.now() }, null, 4));
 
-      // Delay to allow the message to send before restarting
       setTimeout(() => {
         console.log('Exiting process...');
-        process.exit(1); // Exit the process after 2 seconds
+        process.exit(1);
       }, 2000);
     } catch (error) {
       console.error('Error executing the restart command:', error);
